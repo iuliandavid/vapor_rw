da8a2f34000c3c6765aa1a819a059b2f7dac3c4f
iuli <david.iulian@gmail.com>
2017-04-27 14:18:44 +0300

    finished sibling

---
+++ siblings/README.md
@@ -0,0 +1,8 @@
+# Basic Template
+
+Based on Ray Wenderlich's screencast **Server Side Swift with Vapor: Siblings Relations**
+
+## 📖 Documentation
+
+Ses video [video](https://videos.raywenderlich.com/screencasts/598-server-side-swift-with-vapor-sibling-relations) for more details.
+
---
+++ siblings/Sources/App/main.swift
@@ -0,0 +1,62 @@
+import Foundation
+import Vapor
+import VaporPostgreSQL
+import SwiftyBeaverVapor
+import SwiftyBeaver
+import Fluent
+
+let drop = Droplet()
+
+
+// set-up SwiftyBeaver logging destinations (console, file, cloud, ...)
+// learn more at http://bit.ly/2ci4mMX
+let console = ConsoleDestination()  // log to Xcode Console in color
+let file = FileDestination()  // log to file in color
+file.logFileURL = URL(fileURLWithPath: "/tmp/VaporLogs.log") // set log file
+let sbProvider = SwiftyBeaverProvider(destinations: [console, file])
+
+drop.addProvider(sbProvider)
+let log = drop.log.self
+
+// MARK: - Database config
+try drop.addProvider(VaporPostgreSQL.Provider.self)
+drop.preparations.append(Acronym.self)
+drop.preparations.append(TILUser.self)
+drop.preparations.append(Group.self)
+drop.preparations.append(Pivot<TILUser, Group>.self)
+
+(drop.view as? LeafRenderer)?.stem.cache = nil
+// MARK: - Add BasicController
+let basic = BasicController(drop: drop)
+basic.addRoutes()
+
+// MARK: - Add Acronyms Resouce
+let acronyms = AcronymsController(droplet: drop)
+acronyms.addRoutes()
+
+
+let til = TILController(droplet: drop)
+til.addRoutes()
+
+drop.get("combine") { request in
+    guard let rawInput = request.data["input"]?.string else {
+        throw Abort.notFound
+    }
+    let input = try rawInput.validated(by: Count.min(10) && !OnlyAlphanumeric.self)
+    return "Validate \(input.value)"
+}
+
+
+// MARK: - Add ValidatorsController
+let validators = ValidatorsController(drop: drop)
+validators.addRoutes()
+
+// MARK: - Add Users Resouce
+let users = TILUserController(droplet: drop)
+users.addRoutes()
+
+// MARK: - Add Users Resouce
+let groups = GroupController(droplet: drop)
+groups.addRoutes()
+
+drop.run()
